26c1c365d9137289ffba0ade8a169e1259438858
Pedro <devpeu.contact@gmail.com>
2023-02-13 19:20:53 -0300

    adicionado arquivo json de usuarios

--- kontacts/src/components/TableRow/index.js
+++ kontacts/src/components/TableRow/index.js
@@ -4,51 +4,28 @@ import useUtils from "../../hooks/useUtils";
 import "./styles.css";
 
 export default function TableRow({ kontact }) {
-  // const { nome, email, telefone } = kontact;
-  const { setKontactToDelete, setModal, setShowDeleteModal } = useUtils();
-
+  const { nome, email, telefone } = kontact;
+  const { setModal, setShowDeleteModal, setKontactToDelete } = useUtils();
 
   return (
-    // <div className="row-container">
-    //   <span>{nome}</span>
-    //   <span>{email}</span>
-    //   <span>{`(${telefone.slice(0, 2)}) ${telefone.slice(
-    //     2,
-    //     3
-    //   )} ${telefone.slice(3, 7)}-${telefone.slice(7, 11)}`}</span>
-    //   <div className="icons">
-    //     <img
-    //       onClick={() => [setModal({ status: true, type: "edit", kontact })]}
-    //       src={editIcon}
-    //       alt="edit-icon"
-    //     />
-    //     <img
-    //       onClick={() => [
-    //         setKontactToDelete(kontact),
-    //         setShowDeleteModal(true),
-    //       ]}
-    //       src={trashIcon}
-    //       alt="trash-icon"
-    //     />
-    //   </div>
-    // </div>
-    <div onClick={() => [setModal({ status: true, type: "edit", kontact: {nome: "pedro", email: "pedrocadast@Gmail.com", telefone: "38998273349"} })]} className="row-container">
-      <span className="name">Pedro</span>
-      <span >
-        pedrocadast@gmail.com
-      </span>
-      <span>38998273349</span>
+    <div className="row-container">
+      <span>{nome}</span>
+      <span>{email}</span>
+      <span>{`(${telefone.slice(0, 2)}) ${telefone.slice(
+        2,
+        3
+      )} ${telefone.slice(3, 7)}-${telefone.slice(7, 11)}`}</span>
       <div className="icons">
         <img
-          // onClick={() => [setModal({ status: true, type: "edit", kontact })]}
+          onClick={() => [setModal({ status: true, type: "edit", kontact })]}
           src={editIcon}
           alt="edit-icon"
         />
         <img
-          // onClick={() => [
-          //   setKontactToDelete(kontact),
-          //   setShowDeleteModal(true),
-          // ]}
+          onClick={() => [
+            setKontactToDelete(kontact),
+            setShowDeleteModal(true),
+          ]}
           src={trashIcon}
           alt="trash-icon"
         />
---
+++ kontacts/src/utils/users.js
@@ -0,0 +1,22 @@
+const users = [
+  {
+    id: 1,
+    name: "pedro",
+    email: "pedrocadast@gmail.com",
+    tel: "38998273349",
+  },
+  {
+    id: 2,
+    name: "pedro",
+    email: "joao@email.com",
+    tel: "9999999999",
+  },
+  {
+    id: 1,
+    name: "ivy",
+    email: "ivy@emailteste.com",
+    tel: "1111111111",
+  },
+];
+
+export default users;
